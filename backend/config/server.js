73eab777694f74d72f48d964285a05e31c8314f2
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2020-07-26 02:12:22 +0530

    google auth error302 fixed

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
-  url: ''
+  url: 'https://48e84ea973fe.ngrok.io'
 });
 //* use ngrok hosted url. [FOR DEVELOPMENT PURPOSES ONLY]
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'd5faef72-8c3d-4000-af71-61b9e8240b65'
-};
