773f612865f936edd9d9f920616c10c747eddb1a
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2020-07-26 14:46:27 +0530

    dumps updated @ 27-07-2020

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', 'localhost'),
-  port: env.int('PORT', 1337)
+  port: env.int('PORT', 1337),
+  url: ''
 });
 //* use ngrok hosted url. [FOR DEVELOPMENT PURPOSES ONLY]
