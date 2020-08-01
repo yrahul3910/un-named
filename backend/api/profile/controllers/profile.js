3adc340591445d54cec839db93d06c452020955c
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2020-08-02 01:37:54 +0530

    extended update service to handle request if profile belongs to loggedin user

--- backend/api/profile/controllers/profile.js
+++ backend/api/profile/controllers/profile.js
@@ -1,15 +1,37 @@
-'use strict';
+'use strict'
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
   create: async (ctx) => {
     const profile = await strapi.services['profile'].create({
       user: ctx.state.user.id,
       ...ctx.request.body
-    })
-    return profile
+    });
+    return sanitizeEntity(profile, { model: strapi.models.profile });
+  },
+  update: async (ctx) => {
+    const updates = Object.keys(ctx.request.body);
+    const allowed = ['name', 'settings', 'event', 'isLive', 'description'];
+    const isValidUpdates = updates.every(update => allowed.includes(update));
+    if (!isValidUpdates) ctx.response.badRequest('Invalid updates received');
+
+    try {
+      // find profile to update
+      const profile = await strapi.services['profile'].findOne({ _id: ctx.params.id });
+      if (!profile) ctx.response.badRequest('Profile not found');
+      // check if profile belongs to logged-in user
+      if (profile.user.id !== ctx.state.user.id) {
+        ctx.response.badRequest('You are not authorized to update this profile');
+      }
+      // update the profile
+      const updatedProfile = await strapi.services['profile'].update({ _id: ctx.params.id }, { ...ctx.request.body });
+      return sanitizeEntity(updatedProfile, { model: strapi.models.profile });
+    } catch (e) {
+      return e;
+    }
   }
-};
+}
