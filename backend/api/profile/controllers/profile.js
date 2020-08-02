671cb196013a36eb0c89ae61129616fb371331d6
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2020-08-02 12:53:02 +0530

    extended DELETE:profile/:id functionality to handle request from profile owner only

--- backend/api/profile/controllers/profile.js
+++ backend/api/profile/controllers/profile.js
@@ -5,6 +5,13 @@
  * to customize this controller
  */
 const { sanitizeEntity } = require('strapi-utils');
+
+const _isAuthorized = async (_id, user) => {
+  const profile = await strapi.services['profile'].findOne({ _id });
+  if (!profile) return false;
+  return profile.user.id === user.id;
+};
+
 module.exports = {
   create: async (ctx) => {
     const profile = await strapi.services['profile'].create({
@@ -20,16 +27,24 @@ module.exports = {
     if (!isValidUpdates) ctx.response.badRequest('Invalid updates received');
 
     try {
-      // find profile to update
-      const profile = await strapi.services['profile'].findOne({ _id: ctx.params.id });
-      if (!profile) ctx.response.badRequest('Profile not found');
       // check if profile belongs to logged-in user
-      if (profile.user.id !== ctx.state.user.id) {
-        ctx.response.badRequest('You are not authorized to update this profile');
-      }
+      const authorized = await _isAuthorized(ctx.params.id, ctx.state.user);
+      if (!authorized) return ctx.response.badRequest('You are not authorized to update this profile');
+
       // update the profile
       const updatedProfile = await strapi.services['profile'].update({ _id: ctx.params.id }, { ...ctx.request.body });
       return sanitizeEntity(updatedProfile, { model: strapi.models.profile });
+    } catch (e) {
+      return e;
+    }
+  },
+  delete: async (ctx) => {
+    try {
+      const authorized = await _isAuthorized(ctx.params.id, ctx.state.user);
+      if (!authorized) return ctx.response.badRequest('You are not authorized to delete this profile');
+
+      const deletedProfile = await strapi.services['profile'].delete({ _id: ctx.params.id });
+      return sanitizeEntity(deletedProfile, { model: strapi.models.profile });
     } catch (e) {
       return e;
     }
